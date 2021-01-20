989bff40134ca8b7e719ebb29893409637dcd70c
SetiZ <funkysetiz@gmail.com>
2021-01-21 00:50:57 +0100

    debug pdfOptions

--- action.js
+++ action.js
@@ -15,7 +15,7 @@ const puppeteer = require('puppeteer');
                 'displayHeaderFooter': false,
                 'path': outputFile
             };
-            console.log("pdfOptions", core.getInput('pdfOptions'))
+            console.log("pdfOptions", JSON.parse(core.getInput('pdfOptions')))
             const pdfOpts = Object.assign({}, core.getInput('pdfOptions'), pdfDefaults);
             const pptrOpts = {
                 executablePath: googlePath,
